cbfd9ea844e15cd34d4ae2f899722cca7f8ebbf2
Carolina Diaz <carolina.diaz@liesa.com.ar>
2025-08-12 12:46:09 -0300

    PARTIAL - parcel --> vite

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import { Checkbox } from './components/checkbox/checkbox.js';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+console.log("Hellooooooooooooooooooo!!!");
+console.log(Checkbox);
+export { Checkbox };
+
+document.addEventListener('DOMContentLoaded', async () => {
+  let container = document.getElementById('checkbox-demo');
+  if (!container) {
+    //const html = await Checkbox();
+    container = document.createElement('div');
+    container.id = 'checkbox-demo';
+    document.body.appendChild(container);
+  }
+  container.innerHTML = Checkbox();
+});
---
+++ src/components/checkbox/checkbox.js
@@ -0,0 +1,7 @@
+import checkboxHTML from './checkbox.html?raw';
+import './checkbox.scss'; 
+
+export function Checkbox() {
+  console.log("Checkbox component loaded");
+  return checkboxHTML;
+}
