d5254c41fc3b18302e5e8213a43341e154a97479
Carolina Diaz <carolina.diaz@liesa.com.ar>
2025-09-18 10:56:04 -0300

    [partial] - add changes

--- test-app/src/main.js
+++ test-app/src/main.js
@@ -8,20 +8,7 @@ import { LiesaButton, LiesaCheckbox } from '@cdiaz-liesa/ui-kit-liesa'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <liesa-button id="ohana" label="hola">asdasd</liesa-button>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs liesa-form-label">
-      Click on the Vite logo to learn more
-    </p>
+    <liesa-button id="ohana" label="Esto es un cambio muy importante">asdasd</liesa-button>
     <liesa-checkbox></liesa-checkbox>
   </div>
 `
