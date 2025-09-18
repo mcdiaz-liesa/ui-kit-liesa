5e0be46913673afbb1e0683037fb9838c2134d72
Carolina Diaz <carolina.diaz@liesa.com.ar>
2025-09-18 12:00:43 -0300

    add liesa-checkbox id

---
+++ test-app/src/main.js
@@ -0,0 +1,16 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+import { LiesaButton, LiesaCheckbox } from '@cdiaz-liesa/ui-kit-liesa'
+
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <liesa-button id="ohana" label="Esto es un cambio muy importante">asdasd</liesa-button>
+    <liesa-checkbox id="es-un-id"></liesa-checkbox>
+  </div>
+`
+
+setupCounter(document.querySelector('#counter'))
