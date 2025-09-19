5c3bc76510c19ac9d68193e0bdfd503318e4c819
Carolina Diaz <carolina.diaz@liesa.com.ar>
2025-09-19 12:12:22 -0300

    few changes

--- test-app/src/main.js
+++ test-app/src/main.js
@@ -8,7 +8,7 @@ import { LiesaButton, LiesaCheckbox } from '@cdiaz-liesa/ui-kit-liesa'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <liesa-button id="ohana" label="Esto es un cambio muy importante">asdasd</liesa-button>
+    <liesa-button id="ohana" label="Esto es un cambio algo importante">asdasd</liesa-button>
     <liesa-checkbox id="es-un-id"></liesa-checkbox>
   </div>
 `
